b32988407049a43dad5c210a42460e7e233fddbb
sshyta <sshyta@mail.ru>
2025-04-02 23:11:51 +0300

    init

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from PyQt6.QtWidgets import QApplication, QWidget
 import sys
+from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow, QSize
+
+
+class MainWindow (QMainWindow):
+    def __init__ (self):
+        super().__init__()
+        self.setWindowTitle('my app')
+        button = QPushButton('Pushh')
+        self.setCentralWidget(button)
 
 app = QApplication(sys.argv)
 
-windows = QWidget()
+windows = MainWindow()
 windows.show()
 
 # Запуск
