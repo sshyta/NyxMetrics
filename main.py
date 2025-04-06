161b5324c1d4d2864bdc99f28965a529ee390086
sshyta <sshyta@mail.ru>
2025-04-06 18:37:48 +0300

    +oushButton

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import sys
 from PyQt6.QtWidgets import QApplication, QMainWindow
 from ui.ui_main import Ui_MainWindow
-
+from app.core.loaderButton import LoadingFile
 
 class NyxMetric(QMainWindow):
     def __init__(self):
         super(NyxMetric, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-       
-
-      
+        self.ui.retranslateUi(self)
+        
+        self.file_loader = LoadingFile(self)
+        self.ui.pushButton.clicked.connect(self.file_loader.load_file)
+        
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     windows = NyxMetric()
