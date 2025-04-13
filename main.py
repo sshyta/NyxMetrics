8e6272760276851f17a2befe2d27d5038f643e88
sshyta <sshyta@mail.ru>
2025-04-13 22:32:51 +0300

    main fix

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 from PyQt6.QtWidgets import QApplication, QMainWindow
 from ui.ui_main import Ui_MainWindow
 from app.core.loaderButton import LoadingFile
+from app.core.tableViewer import TableViewer
 
 class NyxMetric(QMainWindow):
     def __init__(self):
@@ -13,9 +14,9 @@ class NyxMetric(QMainWindow):
         self.file_loader = LoadingFile(self, self.ui.status_label)
         self.ui.pushButton.clicked.connect(self.file_loader.load_file)
         
+        
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     windows = NyxMetric()
     windows.show()
-    
     sys.exit(app.exec())
