014c51f283f80e443d91f6253ec8c18cbc92f0df
sshyta <sshyta@mail.ru>
2025-04-15 21:50:31 +0300

    data signal

--- app/core/loaderButton.py
+++ app/core/loaderButton.py
@@ -1,10 +1,14 @@
+from PyQt6.QtCore import pyqtSignal, QObject
 from PyQt6.QtWidgets import QFileDialog, QLabel
 import pandas as pd
 
 
-class LoadingFile:
-    def __init__(self, parent_window, status_label=None):
-        self.parent = parent_window
+class LoadingFile(QObject):
+    file_loaded = pyqtSignal(pd.DataFrame) # Сигнал, который будет передавать данные после загрузки
+    
+    def __init__(self, parent, status_label=None):
+        super().__init__(parent)
+        self.parent = parent
         self.status_label = status_label
         self.df = None
 
@@ -24,12 +28,14 @@ class LoadingFile:
         try:
             if file_path.endswith(".csv"):
                 self.df = pd.read_csv(file_path)
-            elif file_path.endswith(".xlsx", ".xls"):
+            elif file_path.endswith((".xlsx", ".xls")):
                 self.df = pd.read_excel(file_path)
             else:
                 if self.status_label:
                     self.status_label.setText("Неподдерживаемый формат")
                 return False, "Формат файла не поддерживается"
+            
+            self.file_loaded.emit(self.df)
             
             if self.status_label:
                 file_name = file_path.split("/")[-1]
