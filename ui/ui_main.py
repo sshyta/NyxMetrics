774699e312c0f70f05abfb32fd49880813d85e96
sshyta <sshyta@mail.ru>
2025-04-27 00:23:38 +0300

    Update: add window (loging/pass), button (sign in/sign up) and wicGl

--- ui/ui_main.py
+++ ui/ui_main.py
@@ -15,9 +15,11 @@ from PyQt6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
+from PyQt6.QtOpenGLWidgets import QOpenGLWidget
 from PyQt6.QtWidgets import (QApplication, QGridLayout, QHBoxLayout, QHeaderView,
-    QLabel, QMainWindow, QPushButton, QSizePolicy,
-    QStackedWidget, QTableView, QWidget)
+    QLabel, QLineEdit, QMainWindow, QPushButton,
+    QSizePolicy, QStackedWidget, QTableView, QVBoxLayout,
+    QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -33,29 +35,64 @@ class Ui_MainWindow(object):
         self.stackedWidget.setGeometry(QRect(0, 50, 1091, 671))
         self.Home = QWidget()
         self.Home.setObjectName(u"Home")
+        self.WICGLWidget = QOpenGLWidget(self.Home)
+        self.WICGLWidget.setObjectName(u"WICGLWidget")
+        self.WICGLWidget.setGeometry(QRect(20, 20, 111, 81))
+        self.layoutWidget = QWidget(self.Home)
+        self.layoutWidget.setObjectName(u"layoutWidget")
+        self.layoutWidget.setGeometry(QRect(830, 282, 171, 61))
+        self.verticalLayout = QVBoxLayout(self.layoutWidget)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.login_line = QLineEdit(self.layoutWidget)
+        self.login_line.setObjectName(u"login_line")
+
+        self.verticalLayout.addWidget(self.login_line)
+
+        self.password_line = QLineEdit(self.layoutWidget)
+        self.password_line.setObjectName(u"password_line")
+
+        self.verticalLayout.addWidget(self.password_line)
+
+        self.layoutWidget1 = QWidget(self.Home)
+        self.layoutWidget1.setObjectName(u"layoutWidget1")
+        self.layoutWidget1.setGeometry(QRect(830, 350, 171, 27))
+        self.horizontalLayout_2 = QHBoxLayout(self.layoutWidget1)
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.horizontalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.sign_in_btn = QPushButton(self.layoutWidget1)
+        self.sign_in_btn.setObjectName(u"sign_in_btn")
+
+        self.horizontalLayout_2.addWidget(self.sign_in_btn)
+
+        self.sign_up_btn = QPushButton(self.layoutWidget1)
+        self.sign_up_btn.setObjectName(u"sign_up_btn")
+
+        self.horizontalLayout_2.addWidget(self.sign_up_btn)
+
         self.stackedWidget.addWidget(self.Home)
         self.Data = QWidget()
         self.Data.setObjectName(u"Data")
         self.tableView = QTableView(self.Data)
         self.tableView.setObjectName(u"tableView")
         self.tableView.setGeometry(QRect(20, 60, 1071, 601))
-        self.layoutWidget = QWidget(self.Data)
-        self.layoutWidget.setObjectName(u"layoutWidget")
-        self.layoutWidget.setGeometry(QRect(20, 30, 361, 27))
-        self.gridLayout = QGridLayout(self.layoutWidget)
+        self.layoutWidget2 = QWidget(self.Data)
+        self.layoutWidget2.setObjectName(u"layoutWidget2")
+        self.layoutWidget2.setGeometry(QRect(20, 30, 361, 27))
+        self.gridLayout = QGridLayout(self.layoutWidget2)
         self.gridLayout.setObjectName(u"gridLayout")
         self.gridLayout.setContentsMargins(0, 0, 0, 0)
-        self.uploadButton = QPushButton(self.layoutWidget)
+        self.uploadButton = QPushButton(self.layoutWidget2)
         self.uploadButton.setObjectName(u"uploadButton")
 
         self.gridLayout.addWidget(self.uploadButton, 0, 0, 1, 1)
 
-        self.status_label = QLabel(self.layoutWidget)
+        self.status_label = QLabel(self.layoutWidget2)
         self.status_label.setObjectName(u"status_label")
 
         self.gridLayout.addWidget(self.status_label, 0, 1, 1, 1)
 
-        self.clearButton = QPushButton(self.layoutWidget)
+        self.clearButton = QPushButton(self.layoutWidget2)
         self.clearButton.setObjectName(u"clearButton")
 
         self.gridLayout.addWidget(self.clearButton, 0, 2, 1, 1)
@@ -67,28 +104,28 @@ class Ui_MainWindow(object):
         self.Settings = QWidget()
         self.Settings.setObjectName(u"Settings")
         self.stackedWidget.addWidget(self.Settings)
-        self.layoutWidget1 = QWidget(self.centralwidget)
-        self.layoutWidget1.setObjectName(u"layoutWidget1")
-        self.layoutWidget1.setGeometry(QRect(20, 10, 340, 27))
-        self.horizontalLayout = QHBoxLayout(self.layoutWidget1)
+        self.layoutWidget3 = QWidget(self.centralwidget)
+        self.layoutWidget3.setObjectName(u"layoutWidget3")
+        self.layoutWidget3.setGeometry(QRect(20, 10, 340, 27))
+        self.horizontalLayout = QHBoxLayout(self.layoutWidget3)
         self.horizontalLayout.setObjectName(u"horizontalLayout")
         self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
-        self.home_btn = QPushButton(self.layoutWidget1)
+        self.home_btn = QPushButton(self.layoutWidget3)
         self.home_btn.setObjectName(u"home_btn")
 
         self.horizontalLayout.addWidget(self.home_btn)
 
-        self.date_btn = QPushButton(self.layoutWidget1)
+        self.date_btn = QPushButton(self.layoutWidget3)
         self.date_btn.setObjectName(u"date_btn")
 
         self.horizontalLayout.addWidget(self.date_btn)
 
-        self.charts_btn = QPushButton(self.layoutWidget1)
+        self.charts_btn = QPushButton(self.layoutWidget3)
         self.charts_btn.setObjectName(u"charts_btn")
 
         self.horizontalLayout.addWidget(self.charts_btn)
 
-        self.settings_btn = QPushButton(self.layoutWidget1)
+        self.settings_btn = QPushButton(self.layoutWidget3)
         self.settings_btn.setObjectName(u"settings_btn")
 
         self.horizontalLayout.addWidget(self.settings_btn)
@@ -97,7 +134,7 @@ class Ui_MainWindow(object):
 
         self.retranslateUi(MainWindow)
 
-        self.stackedWidget.setCurrentIndex(1)
+        self.stackedWidget.setCurrentIndex(0)
 
 
         QMetaObject.connectSlotsByName(MainWindow)
@@ -105,6 +142,12 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"Nyx metrics", None))
+        self.login_line.setText("")
+        self.login_line.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Login...", None))
+        self.password_line.setText("")
+        self.password_line.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Password...", None))
+        self.sign_in_btn.setText(QCoreApplication.translate("MainWindow", u"Sign in", None))
+        self.sign_up_btn.setText(QCoreApplication.translate("MainWindow", u"Sign up", None))
         self.uploadButton.setText(QCoreApplication.translate("MainWindow", u"Upload file...", None))
         self.status_label.setText(QCoreApplication.translate("MainWindow", u"\u0424\u0430\u0439\u043b \u043d\u0435 \u0432\u044b\u0431\u0440\u0430\u043d", None))
         self.clearButton.setText(QCoreApplication.translate("MainWindow", u"\u0421lear", None))
